6c1db28645e3fe5d9cf40d10c920ee08ab5d56fa
maulymd <maulanayusufahmd@gmail.com>
2024-06-11 10:10:46 +0700

    read data

--- webapp/views.py
+++ webapp/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from .models import Makanan
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
 
 def alldata(request):
-    return render(request, 'alldata.html')
+    makanan = Makanan.objects.all()
+    return render(request, "alldata.html", {'makanan':makanan})
